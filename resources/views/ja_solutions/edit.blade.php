be6c2e94a3c547225f174f44dd55eff53000f469
Juniorawan06 <haseebawan.1560@gmail.com>
2022-08-01 18:43:35 +0500

    initial commit

--- resources/views/ja_solutions/edit.blade.php
+++ resources/views/ja_solutions/edit.blade.php
@@ -2,13 +2,13 @@
 @section('title', 'Edit')
 @section('mainarea')
 <div class="container">
-<h1 class="p-3">Edit Staff</h1>
+<h1 class="p-3"  style="font-family: 'Rosalinda Berlinata'">Edit Staff</h1>
 <form method="POST" action="/ja_solutions/edit">
     @csrf()
         <div class="row">
             <input type="hidden" name="id" value="{{$data['id']}}">
             <div class="col-6"><input type="text" class="form-control" name="staffName" placeholder="Username" value="{{$data['staffName']}}"></div>
-            <div class="col-6"><input type="passward" class="form-control" name="staffPass" placeholder="*********"  value="{{$data['staffPass']}}"></div>
+            <div class="col-6"><input type="password" class="form-control" name="staffPass" placeholder="*********"  value="{{$data['staffPass']}}"></div>
             <div class="col-12 p-3"><button type="submit" class="btn btn-success" name="add">Update</button></div>
         </div>
     </div>
--- resources/views/ja_solutions/view.blade.php
+++ resources/views/ja_solutions/view.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'View')
 @section('mainarea')
 <div class="d-flex justify-content-between align-items-center">
-    <h1>Staff List</h1>
+    <h1 style="font-family: 'Rosalinda Berlinata'">Staff List</h1>
     <a href="ja_solutions/add" class="btn btn-primary">+ Add Staff</a>
 </div>
 <table class="table table-sm table-striped table-bordered table-dark table-hover table-responsive">
@@ -17,13 +17,13 @@
         </tr>
     </thead>
     <tbody>
-        @foreach($data as $row)
+        @foreach($data as $dt)
         <tr>
-            <td>{{$row['id']}}</td>
-            <td>{{$row['staffName']}}</td>
-            <td>{{$row['staffPass']}}</td>
-            <td>{{$row['created_at']}}</td>
-            <td>{{$row['updated_at']}}</td>
+            <td>{{$dt['id']}}</td>
+            <td>{{$dt['staffName']}}</td>
+            <td>{{$dt['staffPass']}}</td>
+            <td>{{$dt['created_at']}}</td>
+            <td>{{$dt['updated_at']}}</td>
             <td>
                 <a class="btn btn-success" href="/ja_solutions/edit/{{$dt['id']}}">Edit</a>
                 <a class="btn btn-danger" href="/ja_solutions/delete/{{$dt['id']}}">Trash</a>
