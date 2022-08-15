8177db8af6a93bc92206b4869528902caa505ff5
Juniorawan06 <haseebawan.1560@gmail.com>
2022-08-16 03:07:58 +0500

    fixing bug

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', [staffsController::class, 'view']);
 
 Route::get('/ja_solutions', [staffsController::class, 'view']);
 Route::get('/ja_solutions/delete/{id}', [staffsController::class, 'delete']);
-Route::view('ja_solutions/add', 'ja_solutions/add');
-Route::post('/ja_solutions/add', [staffsController::class, 'add']);
+Route::view('/add', 'ja_solutions/add')->name('add');
+Route::post('/add', [staffsController::class, 'add']);
 Route::get('/ja_solutions/edit/{id}', [staffsController::class, 'update']);
 Route::post('/ja_solutions/edit', [staffsController::class, 'change']);
